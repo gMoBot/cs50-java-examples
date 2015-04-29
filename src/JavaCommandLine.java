e00ec84651b7af272394c0e73b3f739ddb042101
gMoBot <gMoBot@users.noreply.github.com>
2015-04-29 14:39:01 -0500

    adding in JavaCommandLine examples

--- src/JavaCommandLine.java
+++ src/JavaCommandLine.java
@@ -1,16 +1,20 @@
+
 import java.io.Console;
 
-
+/**
+ * Created by IntelliJ IDEA.
+ * User: mpmenne
+ * Date: 3/17/14
+ * Time: 6:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
 public class JavaCommandLine {
 
     public static void main(String[] varArgs) {
-        System.out.println("Hello I am a computer");
-
+        System.out.println("Hello, I am a computer");
+        
         Console c = System.console();
-        String name = c.readLine("What is your name?? ");
+        String name = c.readLine("What is your name? ");
         System.out.println("Hello " + name);
-
-
     }
-
 }
